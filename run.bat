44ae79f689bdb7af2ca3b884e9464c530195043b
anushra <anushra.singh@moneyview.in>
2022-06-19 20:55:36 +0530

    extent report added

---
+++ run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\91720\git\AutomationTask
+mvn clean install
--- src/test/java/testCases/BaseClass.java
+++ src/test/java/testCases/BaseClass.java
@@ -4,13 +4,29 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.Timeouts;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 import utilities.ReadConfig;
 
@@ -20,16 +36,16 @@ ReadConfig readconfig=new ReadConfig();
 	
 	public String baseURL=readconfig.getApplicationURL();
 	public static WebDriver driver;
+	public Logger logger;
 	
-	public static Logger logger;
+	
 	
 	@Parameters("browser")
-	@BeforeClass
+	@BeforeSuite
 	public void setup(String br)
 	{			
 		logger = Logger.getLogger("Login to amazon site");
 		PropertyConfigurator.configure("Log4j.properties");
-		
 		if(br.equals("chrome"))
 		{
 			System.setProperty("webdriver.chrome.driver",readconfig.getChromePath());
@@ -46,12 +62,13 @@ ReadConfig readconfig=new ReadConfig();
 			driver = new InternetExplorerDriver();
 		}
 		
-		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.manage().window().maximize();		
 	}
 	
-	@AfterClass
-	public void tearDown()
+	@AfterSuite
+	public void close()
 	{
+		
 		driver.quit();
 	}	
 
